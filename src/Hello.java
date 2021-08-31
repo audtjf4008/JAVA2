847cdc31840c2be7f8c46f26818eef6864f24b82
Mac_1 <Mac_1@DESKTOP-P3QHQ70>
2021-09-01 08:11:05 +0900

    11

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello java world!!!");
-		System.out.println("my name is kimsam");
+		System.out.println("my name is Kim myeong seol");
 	}
 }
