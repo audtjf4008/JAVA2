f7ece0e22973589dd1c9e6e8876af131fb0a0cde
Mac_1 <Mac_1@DESKTOP-P3QHQ70>
2021-09-06 15:20:59 +0900

    0906

---
+++ src/Light.java
@@ -0,0 +1,14 @@
+
+public class Light {
+	public static void main(String[] args) {
+		long lightSpeed;
+		long distance;
+		
+		lightSpeed = 300000;
+		distance = lightSpeed * 365L * 24 * 60 * 60;
+		
+		System.out.println("빛이 1년 동안 가는거리 " + distance);
+		
+	}
+
+}
